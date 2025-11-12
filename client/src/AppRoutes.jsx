7f979a588447a50996add325e395d864b1afa9a3
Noa123715 <noa123715@gmail.com>
2025-11-12 15:56:33 +0200

    add layout

--- client/src/AppRoutes.jsx
+++ client/src/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-// import Layout from "./components/Layout";
+import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./components/About";
 import TermsModal from "./components/Terms";
@@ -7,30 +7,15 @@ import LoginPage from "./pages/LoginPage";
 
 export default function AppRoutes() {
     return (
-        // <Router>
-        //     <Routes>
-        //         {/* Login route - without Layout */}
-        //         <Route path="/" element={<LoginPage />} />
-
-        //         {/* All other routes - with Layout */}
-        //         <Route path="/home" element={<Layout>  <HomePage /> </Layout>} />
-        //         <Route path="/about" element={<Layout><AboutPage /></Layout>} />
-        //         <Route path="/terms" element={<Layout> <TermsModal /> </Layout>} />
-
-        //         {/* Redirect to home */}
-        //         <Route path="*" element={<Navigate to="/" />} />
-        //     </Routes>
-        // </Router>
-
         <Router>
             <Routes>
                 {/* Login route - without Layout */}
                 <Route path="/" element={<LoginPage />} />
 
                 {/* All other routes - with Layout */}
-                <Route path="/home" element={<HomePage />} />
-                <Route path="/about" element={<AboutPage />} />
-                <Route path="/terms" element={<TermsModal />} />
+                <Route path="/home" element={<Layout>  <HomePage /> </Layout>} />
+                <Route path="/about" element={<Layout><AboutPage /></Layout>} />
+                <Route path="/terms" element={<Layout> <TermsModal /> </Layout>} />
 
                 {/* Redirect to home */}
                 <Route path="*" element={<Navigate to="/" />} />
