7d11958ea9cdfea187ecd3354a391dc0ea6e5d71
ChaniChalamish <b0527626958@gmail.com>
2025-12-22 11:13:15 +0200

    cookie praser in redis was missing

--- Redis_Server/index.js
+++ Redis_Server/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
 import pageRoutes from "./routes/pageRoutes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
